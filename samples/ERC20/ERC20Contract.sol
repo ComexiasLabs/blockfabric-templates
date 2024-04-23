62f52ef3515ce5c84fa2d5aa0e4f61e50e9a0c43
Jeffrey Lewis <novensiles@gmail.com>
2024-04-23 21:07:00 +0800

    Fix contract name for erc20 sample

--- samples/ERC20/ERC20Contract.sol
+++ samples/ERC20/ERC20Contract.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC721Contract is ERC20 {
+contract ERC20Contract is ERC20 {
     constructor(uint256 initialSupply) ERC20("MySampleToken", "MST") {
         _mint(msg.sender, initialSupply);
     }
